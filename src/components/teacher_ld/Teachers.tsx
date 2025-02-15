54aca780c71ef774f786c888d2332c2e9763a7bb
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-02-16 02:51:36 +0700

    update the edit teacher page again

--- src/components/teacher_ld/Teachers.tsx
+++ src/components/teacher_ld/Teachers.tsx
@@ -347,149 +347,153 @@ export default function BasicTableOne() {
 
             <Modal isOpen={isEditOpen} onClose={closeEditModal} className="max-w-[1000px] p-5 lg:p-10">
                 <h2 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">Edit Teacher</h2>
-                <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
-                    Teacher Information
-                </h4>
+                {selectedActivity && (
+                    <div>
+                        <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
+                            Teacher Information
+                        </h4>
 
-                <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-5">
-                    <div className="col-span-1">
-                        <Label>Situation</Label>
-                        <select className="px-6 py-3 bg-gray-900 text-gray-600 border rounded-lg text-xs w-1/2">
-                            <option value="">All</option>
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                        </select>
+                        <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-5">
+                            <div className="col-span-1">
+                                <Label>Situation</Label>
+                                <select className="px-6 py-3 bg-gray-900 text-gray-600 border rounded-lg text-xs w-1/2">
+                                    <option value="">All</option>
+                                    <option value="1">Option 1</option>
+                                    <option value="2">Option 2</option>
+                                </select>
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>First Name*:</Label>
+                                <Input type="text" placeholder="Hasan" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Last Name*:</Label>
+                                <Input type="email" placeholder="Ali" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Email:</Label>
+                                <Input type="text" placeholder="hasaneducationadvisor@gmail.com" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Email:</Label>
+                                <input type="checkbox" className="mr-2" placeholder="Team Manager" />
+                                <label className="text-sm font-medium text-gray-700 dark:text-gray-300">Email Opt Out:</label>
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Home Phone:</Label>
+                                <Input type="text" placeholder="+09 363 398 46" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Mobile Phone:</Label>
+                                <Input type="text" placeholder="+09 363 398 46" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Other Phone:</Label>
+                                <Input type="text" placeholder="+09 363 398 46" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Year:</Label>
+                                <Input type="text" placeholder="Team Manager" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Choose Your School:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Department:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Title:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Website:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+                        </div>
+
+                        {/* Line Separator */}
+                        <hr className="my-6" />
+
+                        {/* Address Information */}
+
+                        <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
+                            Address Information
+                        </h4>
+
+                        <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
+                            <div className="col-span-1">
+                                <Label>Street Address 1:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Street Address 2:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>City:</Label>
+                                <Input type="email" placeholder="England" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>County:</Label>
+                                <Input type="text" placeholder="" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Postal Code:</Label>
+                                <Input type="text" placeholder="29407" />
+                            </div>
+
+                            <div className="col-span-1">
+                                <Label>Country:</Label>
+                                <Input type="email" placeholder="United Kingdom" />
+                            </div>
+                        </div>
+
+                        {/* Line Separator */}
+                        <hr className="my-6" />
+
+                        {/* Description Information */}
+
+                        <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
+                            Description Information
+                        </h4>
+
+                        <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-3">
+                            <div className="col-span-1">
+                                <textarea rows={6} cols={80} className="bg-gray-900 text-gray-600border rounded-lg" placeholder="Type your message here..." />
+                            </div>
+                        </div>
+
+                        {/* Line Separator */}
+                        <hr className="my-2" />
+
+                        <div className="flex items-center justify-end w-full gap-3 mt-6">
+                            <Button size="sm" variant="outline">
+                                Cancel
+                            </Button>
+                            <Button size="sm">
+                                Save
+                            </Button>
+                        </div>
                     </div>
-
-                    <div className="col-span-1">
-                        <Label>First Name*:</Label>
-                        <Input type="text" placeholder="Hasan" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Last Name*:</Label>
-                        <Input type="email" placeholder="Ali" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Email:</Label>
-                        <Input type="text" placeholder="hasaneducationadvisor@gmail.com" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Email:</Label>
-                        <input type="checkbox" className="mr-2" placeholder="Team Manager" />
-                        <label className="text-sm font-medium text-gray-700 dark:text-gray-300">Email Opt Out:</label>
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Home Phone:</Label>
-                        <Input type="text" placeholder="+09 363 398 46" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Mobile Phone:</Label>
-                        <Input type="text" placeholder="+09 363 398 46" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Other Phone:</Label>
-                        <Input type="text" placeholder="+09 363 398 46" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Year:</Label>
-                        <Input type="text" placeholder="Team Manager" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Choose Your School:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Department:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Title:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Website:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-                </div>
-
-                {/* Line Separator */}
-                <hr className="my-6" />
-
-                {/* Address Information */}
-
-                <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
-                    Address Information
-                </h4>
-
-                <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
-                    <div className="col-span-1">
-                        <Label>Street Address 1:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Street Address 2:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>City:</Label>
-                        <Input type="email" placeholder="England" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>County:</Label>
-                        <Input type="text" placeholder="" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Postal Code:</Label>
-                        <Input type="text" placeholder="29407" />
-                    </div>
-
-                    <div className="col-span-1">
-                        <Label>Country:</Label>
-                        <Input type="email" placeholder="United Kingdom" />
-                    </div>
-                </div>
-
-                {/* Line Separator */}
-                <hr className="my-6" />
-
-                {/* Description Information */}
-
-                <h4 className="mb-2 text-lg font-medium text-gray-800 dark:text-white/90">
-                    Description Information
-                </h4>
-
-                <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-3">
-                    <div className="col-span-1">
-                        <textarea rows={6} cols={80} className="bg-gray-900 text-gray-600border rounded-lg" placeholder="Type your message here..." />
-                    </div>
-                </div>
-
-                {/* Line Separator */}
-                <hr className="my-2" />
-
-                <div className="flex items-center justify-end w-full gap-3 mt-6">
-                    <Button size="sm" variant="outline">
-                        Cancel
-                    </Button>
-                    <Button size="sm">
-                        Save
-                    </Button>
-                </div>
+                )}
             </Modal>
         </div>
     );
