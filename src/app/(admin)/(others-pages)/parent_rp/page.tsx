f492de7d42af33ee63691471a447cc297de9c561
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-02-19 14:04:51 +0700

    update parent report section

--- src/app/(admin)/(others-pages)/parent_rp/page.tsx
+++ src/app/(admin)/(others-pages)/parent_rp/page.tsx
@@ -1,17 +1,17 @@
-import Reports from "@/components/leadtrack_rp/Reports";
+import Reports from "@/components/parent_rp/Reports";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "LEM Admin | Lead Tracking Reports",
+  title: "LEM Admin | Parent Communication Report",
   description:
-    "This is Lead Tracking page of Admin",
+    "This is Parent Communication of Admin",
 };
 export default function page() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Lead Tracking Reports" />
+      <PageBreadcrumb pageTitle="Parent Communication Report" />
       { <Reports /> }
     </div>
   );
