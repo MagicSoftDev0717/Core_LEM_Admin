2871b528475e195d2abc49875576c592000d7a6e
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-02-28 04:20:39 +0700

    update the student page

--- src/components/students/Students.tsx
+++ src/components/students/Students.tsx
@@ -738,7 +738,7 @@ export default function BasicTableOne() {
 
             {/* Export to Excel button (Right aligned) */}
             <div className="flex justify-end">
-              <button
+              <button onClick={exportToExcel}
                 className="px-4 py-2 bg-blue-600 text-white rounded-lg"
               >
                 Export to Excel
