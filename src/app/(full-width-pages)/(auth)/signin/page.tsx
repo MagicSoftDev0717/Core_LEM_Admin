a3566fceaa9838f00f1d07beaea7c9931f03410e
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-03 15:42:33 +0700

    update the dashboard page

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -26,7 +26,7 @@ export default function SignIn() {
             />
           </Link>
           <p className="text-center text-gray-400 dark:text-white/60">
-            Free and Open-Source Tailwind CSS Admin Dashboard Template
+            Core-LearnEnglishMaths
           </p>
         </div>
       </div>
--- src/app/(full-width-pages)/(auth)/signup/page.tsx
+++ src/app/(full-width-pages)/(auth)/signup/page.tsx
@@ -28,7 +28,7 @@ export default function SignUp() {
             />
           </Link>
           <p className="text-center text-gray-400 dark:text-white/60">
-            Free and Open-Source Tailwind CSS Admin Dashboard Template
+          Core-LearnEnglishMaths
           </p>
         </div>
       </div>
