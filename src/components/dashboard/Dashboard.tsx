0a00c400ba5f353a52423d1abb4aa192e09fb35b
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-04 17:12:50 +0700

    update dashboard page

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -21,7 +21,7 @@ export default function Dashboard() {
 
 
     return (
-        <div>
+        <div style={{fontFamily: 'Arial'}}>
             <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6 mb-2">
                 <div className="space-y-6">
                     {/* type tilebar code */}
@@ -58,8 +58,8 @@ export default function Dashboard() {
                 </div>
 
                 <div className="col-span-12 space-y-6 xl:col-span-8">
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md min-h-32 flex items-center justify-between">
+                    <div className="grid grid-cols-2 gap-2">
+                        <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md min-h-30 flex items-center justify-between">
                             <div className="w-5/6">
                                 <div className="text-lg font-semibold text-gray-900 dark:text-white pb-2">Students</div>
                                 <div className="pt-2 text-gray-600 dark:text-gray-400 grid grid-cols-3 text-center">
@@ -134,11 +134,13 @@ export default function Dashboard() {
                                 </div>
                             </div>
                             <div className="w-1/6 flex justify-center">
-                                <Button className="px-4 py-2"> <CreditCard  className="w-5 h-5" /></Button>
+                                <Button className="px-4 py-2"> <CreditCard className="w-5 h-5" /></Button>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
 
                 <div className="col-span-12">
                     {/* <EcommerceMetrics /> */}
@@ -150,6 +152,182 @@ export default function Dashboard() {
 
                 <div className="col-span-12 xl:col-span-7">
                     {/* <RecentOrders /> */}
+                </div>
+            </div>
+            <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-2 mb-2">
+                <div className="flex items-center space-x-4"> {/* Use flexbox for horizontal alignment */}
+                    {/* Display the text */}
+                    <h2 className="text-lg font-semibold text-gray-900 dark:text-white">Academics & LMS Data Overview</h2>
+
+                    {/* Combo box (Select dropdown) */}
+                    <select className="p-2 border border-gray-300 rounded-lg bg-white dark:bg-gray-800 dark:text-gray-300">
+                        <option value="">Default Academic Sesstion</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 md:gap-6">
+
+                <div className="col-span-12 space-y-6 xl:col-span-6">
+                    <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md">
+                        <div className="grid grid-cols-3 gap-2">
+                            {/* Grid Item 1 */}
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/class.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Class</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Grid Item 2 */}
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/section.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Section</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/subject.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Subject</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/flip.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Flip Class</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/discuss.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Discussion</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/assess.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Assessment</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/quiz.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Quizzes</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/live.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Live Lectures</span>
+                                    </div>
+                                </div>
+                            </div><div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex items-center space-x-2"> {/* Reduced space between the icon and text */}
+                                    {/* Left side: Icon */}
+                                    <div className="flex items-center justif-bottom h-20 w-20">
+                                        <img src="/assets/academic/resource.svg" alt="SVG Icon" className="h-full w-full object-contain" />
+                                    </div>
+
+                                    {/* Right side: Number and Text */}
+                                    <div className="flex flex-col items-middle justify-left h-20 w-2 space-y-1">
+                                        {/* Number on top */}
+                                        <span className="text-2xl font-semibold dark:text-gray-300">1</span>
+                                        {/* Text on bottom */}
+                                        <span className="text-gray-600 text-sm dark:text-gray-400">Resources</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-12 space-y-6 xl:col-span-6">
+                    <div className="grid grid-cols-1 gap-2">
+                        <div className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md min-h-96 flex items-center justify-between">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
