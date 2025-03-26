9bcfd8ed359938e1e2c11a4ca365d8af59f13c18
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-27 00:21:16 +0700

    update mysql prisma #11

--- prisma123/migrations/20250305181757_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `parent_id` to the `Student` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `student` ADD COLUMN `parent_id` INTEGER NOT NULL;
