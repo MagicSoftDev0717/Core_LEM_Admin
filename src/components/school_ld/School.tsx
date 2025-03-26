9809b5c72757d0e56c55a2b82dda10b27bbc8e39
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-26 16:58:50 +0700

    update for school page

--- src/components/school_ld/School.tsx
+++ src/components/school_ld/School.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/themes/light.css";
